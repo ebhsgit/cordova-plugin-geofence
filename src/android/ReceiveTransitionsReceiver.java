83bc85a7393f1009705c189c7fc9193c4cd79bb7
8bhsolutions <48874658+8bhsolutions@users.noreply.github.com>
2020-08-04 12:51:04 +1000

    Geofence - Fix internal error not being broadcast

--- src/android/ReceiveTransitionsReceiver.java
+++ src/android/ReceiveTransitionsReceiver.java
@@ -52,109 +52,118 @@ public class ReceiveTransitionsReceiver extends BroadcastReceiver {
                 context
         );
 
-        // TODO: refactor this, too long
-        // First check for errors
-        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
-        if (geofencingEvent.hasError()) {
-            // Get the error code with a static method
-            int errorCode = geofencingEvent.getErrorCode();
-            String error = "Location Services error: " + Integer.toString(errorCode);
-            // Log the error
-            logger.log(Log.ERROR, error);
-            broadcastIntent.putExtra("error", error);
-        } else {
-            // Get the type of transition (entry or exit)
-            int transitionType = geofencingEvent.getGeofenceTransition();
-
-            List<Geofence> triggerList = geofencingEvent.getTriggeringGeofences();
-            List<GeoNotification> geoNotifications = new ArrayList<>();
-            for (Geofence fence : triggerList) {
-                String fenceId = fence.getRequestId();
-                GeoNotification geoNotification = store
-                        .getGeoNotification(fenceId);
-
-                if (geoNotification != null && !GeofencePlugin.isSnoozed(geoNotification.id) && geoNotification.isWithinTimeRange()) {
-                    geoNotification.transitionType = transitionType;
-                    geoNotifications.add(geoNotification);
-                }
-            }
-
-            if (geofencingEvent.getTriggeringLocation() != null) {
-                broadcastIntent.putExtra("triggerLocation", geofencingEvent.getTriggeringLocation());
-            }
-
-            if (transitionType == Geofence.GEOFENCE_TRANSITION_ENTER
-                    || transitionType == Geofence.GEOFENCE_TRANSITION_EXIT) {
-                logger.log(Log.DEBUG, "Geofence transition detected");
-
-                if (geoNotifications.size() > 0) {
-                    for (GeoNotification geoNotification : geoNotifications) {
-                        if (geoNotification.notification != null && geoNotification.notification.canBeTriggered()) {
-                            this.updateLastTriggeredByNotificationId(geoNotification.notification.id, geoNotifications);
-                            notifier.notify(geoNotification.notification,
-                                    transitionType == Geofence.GEOFENCE_TRANSITION_ENTER ? "enter" : "exit");
-                            logger.log(Log.DEBUG, "Notification sent");
-                        } else {
-                            logger.log(Log.DEBUG, "Frequency control. Skip notification");
-                        }
-                    }
-
-                    broadcastIntent.putExtra("transitionData", Gson.get().toJson(geoNotifications));
-                    GeofencePlugin.onTransitionReceived(geoNotifications);
-                }
-            } else if (transitionType == Geofence.GEOFENCE_TRANSITION_DWELL) {
-                logger.log(Log.DEBUG, "Geofence transition dwell detected");
-
-                if (geoNotifications.size() > 0) {
-                    broadcastIntent.putExtra("transitionData", Gson.get().toJson(geoNotifications));
-                    GeofencePlugin.onTransitionReceived(geoNotifications);
-                }
-            } else {
-                String error = "Geofence transition error: " + transitionType;
+        try {
+            // TODO: refactor this, too long
+            // First check for errors
+            GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+            if (geofencingEvent.hasError()) {
+                // Get the error code with a static method
+                int errorCode = geofencingEvent.getErrorCode();
+                String error = "Location Services error: " + Integer.toString(errorCode);
+                // Log the error
                 logger.log(Log.ERROR, error);
                 broadcastIntent.putExtra("error", error);
             }
+            else {
+                // Get the type of transition (entry or exit)
+                int transitionType = geofencingEvent.getGeofenceTransition();
 
-            broadcastIntent.putExtra("trigger_listSize", triggerList.size());
-            broadcastIntent.putExtra("trigger_transitionType", transitionType);
-            
-            context.sendBroadcast(broadcastIntent);
+                List<Geofence> triggerList = geofencingEvent.getTriggeringGeofences();
+                List<GeoNotification> geoNotifications = new ArrayList<>();
+                for (Geofence fence : triggerList) {
+                    String fenceId = fence.getRequestId();
+                    GeoNotification geoNotification = store
+                            .getGeoNotification(fenceId);
 
-            for (GeoNotification geoNotification : geoNotifications) {
-                if (geoNotification.url != null) {
-                    String transition = null;
-                    if (transitionType == Geofence.GEOFENCE_TRANSITION_ENTER)
-                        transition = "ENTER";
-                    if (transitionType == Geofence.GEOFENCE_TRANSITION_DWELL)
-                        transition = "DWELL";
-                    if (transitionType == Geofence.GEOFENCE_TRANSITION_EXIT)
-                        transition = "EXIT";
+                    if (geoNotification != null && !GeofencePlugin.isSnoozed(geoNotification.id) && geoNotification.isWithinTimeRange()) {
+                        geoNotification.transitionType = transitionType;
+                        geoNotifications.add(geoNotification);
+                    }
+                }
 
-                    PersistableBundle bundle = new PersistableBundle();
-                    bundle.putString("id", geoNotification.id);
-                    bundle.putString("url", geoNotification.url);
-                    bundle.putString("authorization", geoNotification.authorization);
-                    bundle.putString("transition", transition);
+                if (geofencingEvent.getTriggeringLocation() != null) {
+                    broadcastIntent.putExtra("triggerLocation", geofencingEvent.getTriggeringLocation());
+                }
 
-					TimeZone tz = TimeZone.getTimeZone("UTC");
-					DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-					df.setTimeZone(tz);
-                    bundle.putString("date", df.format(new Date()));
+                if (transitionType == Geofence.GEOFENCE_TRANSITION_ENTER
+                        || transitionType == Geofence.GEOFENCE_TRANSITION_EXIT) {
+                    logger.log(Log.DEBUG, "Geofence transition detected");
 
-                    Log.i(GeofencePlugin.TAG, "Scheduling job for " + geoNotification.toJson());
+                    if (geoNotifications.size() > 0) {
+                        for (GeoNotification geoNotification : geoNotifications) {
+                            if (geoNotification.notification != null && geoNotification.notification.canBeTriggered()) {
+                                this.updateLastTriggeredByNotificationId(geoNotification.notification.id, geoNotifications);
+                                notifier.notify(geoNotification.notification,
+                                        transitionType == Geofence.GEOFENCE_TRANSITION_ENTER ? "enter" : "exit");
+                                logger.log(Log.DEBUG, "Notification sent");
+                            }
+                            else {
+                                logger.log(Log.DEBUG, "Frequency control. Skip notification");
+                            }
+                        }
 
-                    JobScheduler jobScheduler =
-                            (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
-                    jobScheduler.schedule(
-                            new JobInfo.Builder(1, new ComponentName(context, TransitionJobService.class))
-                                    .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
-                                    .setExtras(bundle)
-                                    .build()
-                    );
+                        broadcastIntent.putExtra("transitionData", Gson.get().toJson(geoNotifications));
+                        GeofencePlugin.onTransitionReceived(geoNotifications);
+                    }
+                }
+                else if (transitionType == Geofence.GEOFENCE_TRANSITION_DWELL) {
+                    logger.log(Log.DEBUG, "Geofence transition dwell detected");
+
+                    if (geoNotifications.size() > 0) {
+                        broadcastIntent.putExtra("transitionData", Gson.get().toJson(geoNotifications));
+                        GeofencePlugin.onTransitionReceived(geoNotifications);
+                    }
+                }
+                else {
+                    String error = "Geofence transition error: " + transitionType;
+                    logger.log(Log.ERROR, error);
+                    broadcastIntent.putExtra("error", error);
+                }
+
+                broadcastIntent.putExtra("trigger_listSize", triggerList.size());
+                broadcastIntent.putExtra("trigger_transitionType", transitionType);
+
+                for (GeoNotification geoNotification : geoNotifications) {
+                    if (geoNotification.url != null) {
+                        String transition = null;
+                        if (transitionType == Geofence.GEOFENCE_TRANSITION_ENTER)
+                            transition = "ENTER";
+                        if (transitionType == Geofence.GEOFENCE_TRANSITION_DWELL)
+                            transition = "DWELL";
+                        if (transitionType == Geofence.GEOFENCE_TRANSITION_EXIT)
+                            transition = "EXIT";
+
+                        PersistableBundle bundle = new PersistableBundle();
+                        bundle.putString("id", geoNotification.id);
+                        bundle.putString("url", geoNotification.url);
+                        bundle.putString("authorization", geoNotification.authorization);
+                        bundle.putString("transition", transition);
+
+                        TimeZone tz = TimeZone.getTimeZone("UTC");
+                        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+                        df.setTimeZone(tz);
+                        bundle.putString("date", df.format(new Date()));
+
+                        Log.i(GeofencePlugin.TAG, "Scheduling job for " + geoNotification.toJson());
+
+                        JobScheduler jobScheduler =
+                                (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+                        jobScheduler.schedule(
+                                new JobInfo.Builder(1, new ComponentName(context, TransitionJobService.class))
+                                        .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                                        .setExtras(bundle)
+                                        .build()
+                        );
+                    }
                 }
             }
         }
+        catch (Exception e) {
+            logger.log(e.getMessage(), e);
+            broadcastIntent.putExtra("error", e.getMessage());
+        }
 
+        context.sendBroadcast(broadcastIntent);
     }
 
     private void updateLastTriggeredByNotificationId(int id, List<GeoNotification> geoList) {
